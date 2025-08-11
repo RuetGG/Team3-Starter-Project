27deee15f0af29ebe844ebd9a67122184172757d
SamuelAsmare <samuelasmare19@gmail.com>
2025-08-11 12:22:23 +0300

    feat_reviewer_detail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@ import AdminCyclePage from './auth/signup/admin/admin_cycles/page'
 import RegistrationPage from './auth/signup/page'
 import ProfilePage from './auth/signup/profile/page'
 import LoginPage from './auth/signin/page' 
-import AdminUser from '../app/auth/signup/admin/user/page'
-import AdminUserForm from './auth/signup/admin/admin_user_form/page'
+import ReviewerDetail from '../app/auth/signup/reviewer/reviewer_detail'
 export default function HomePage() {
   return (
     <>
-    <AdminUserForm/>
+    <ReviewerDetail/>
     </>
    
   )
